c1f4f8b90582d42bbf9c2d0431883682e5bd41ac
Mohiuddin Faruqe <faruqem@yahoo.com>
2016-09-13 18:26:15 -0400

    update README.md

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-#CSCIE -15 Dynamic Web Applications - Project P1
+#HSE CSCIE -15 Dynamic Web Applications - Project P1
 
 ---
 
 ###Author: Mohiuddin Faruqe
 ###Create Date: Sept 10, 2016
-###Synopsis: This is the portfolio website for Harvard Extension School CSCIE-15 Dynamic Web 
-Applications course projects.
+###Synopsis: This is the portfolio website for Harvard Extension School CSCIE-15 Dynamic Web Applications course projects.
 
 ---
 Site Link: [Project P1](http://p1.guddi.ca)
--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<title>Project P1</title>
-</head>
-<body>
-	<h1>
-		<?php
-			echo "This is project P1.";
-		?>
-	</h1>
-</body>
+  <head>
+    <meta charset ="utf-8">
+    <title>Project P1</title>
+
+    <!--Bootstrap CDNs-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!--Local CSS file-->
+    <link rel="stylesheet" href="css/my_style.css">
+  <head>
+  <body>
+    <h1>
+    	<?php echo "Project P1"; ?>
+    </h1>
+    <img src="images/mo.jpg" alt="Picture of Mo">
+
+    <!--Local JS File-->
+    <script src="js/my_script.js"></script>
+    <!-- jQuery CDN -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <!-- Bootstrap Latest compiled and minified JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <body>
 </html>
