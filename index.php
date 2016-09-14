d717566542bb0fc780c8accff8af374d6733fe8c
Mohiuddin Faruqe <faruqem@yahoo.com>
2016-09-13 22:18:53 -0400

    Update index.php, incorporate bootstrap framework, add local CSS and image.

--- index.php
+++ index.php
@@ -12,11 +12,48 @@
     <link rel="stylesheet" href="css/my_style.css">
   <head>
   <body>
-    <h1>
-    	<?php echo "Project P1"; ?>
-    </h1>
-    <img src="images/mo.jpg" alt="Picture of Mo">
+    <!--<div class="jumbotron text-center">
+      <h1>Mohiuddin Faruqe</h1>
+      <p>Project P1: System Setup / Portfolio</p>
+    </div>-->
+    <div class="container">
+      <div class="container-fluid bg-1 text-center bottom-buffer">
+          <img src="images/mo.jpg" class="img-responsive img-circle" alt="Picture of Mo" width="260" height="280">
+          <h1>Mohiuddin Faruqe</h1>
+      </div>
+      <div class="row">
+        <div class="col-lg-4">
+            <h2>Project P2</h2>
+            <h4><a href="p2.guddi.ca">View Project P2</a></h4>
+            <h4><a href="https://github.com/faruqem/p2">Project P2 Github Link</a></h4>
+            <p>The goal of this project is to give hands-on practice with working with the basics of PHP.</p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Project P3</h2>
+            <h4><a href="p3.guddi.ca">View Project P3</a></h4>
+            <h4><a href="https://github.com/faruqem/p3">Project P3 Github Link</a></h4>
+            <p>This project is to give hands-on practice with working with the essentials of building a Laravel-based site.</p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Project P4</h2>
+            <h4><a href="p4.guddi.ca">View Project P4</a></h4>
+            <h4><a href="https://github.com/faruqem/p4">Project P4 Github Link</a></h4>
+            <p>The final project is to bring together all the skills learned throughout the semester to create a web app.</p>
+        </div>
+      </div>
 
+      <!--<nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Menu</a>
+          </div>
+        </div>
+      </nav>-->
+      <div class="container-fluid bg-1 text-center top-buffer">
+        <h3>Project P1 - Setup and Portfolio</h3>
+        <h3>CSCI E-15 Dynamic Web Applications Course Projects, Harvard Extension School</h3>
+      </div>
+    </div>
     <!--Local JS File-->
     <script src="js/my_script.js"></script>
     <!-- jQuery CDN -->
